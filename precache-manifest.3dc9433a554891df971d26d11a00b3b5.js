1734e867083ad59966a86e0dc71f0171d55af707
Juan Cabello <jdcabelloa@outlook.com>
2020-07-14 18:42:52 +1200

    Updates

---
+++ precache-manifest.3dc9433a554891df971d26d11a00b3b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62079524ee8463445108e3770e0ffdc",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "a3c87166ab4ebb7c4d5c",
+    "url": "/my-react-portfolio/static/js/2.8d424c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-portfolio/static/js/2.8d424c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26c0cbe60211f3eef88",
+    "url": "/my-react-portfolio/static/js/main.17755381.chunk.js"
+  },
+  {
+    "revision": "49e3a1ff902bb41350c9",
+    "url": "/my-react-portfolio/static/js/runtime-main.04f9f533.js"
+  }
+]);
