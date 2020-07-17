2372b0895aae386575bdd1f14b5ddb8d890f111b
juancabello <42507624+juancabellodev@users.noreply.github.com>
2020-07-17 14:02:02 +1200

    Replace form for Contact me button

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -17,56 +17,12 @@ function ContactUs({ resumeData }) {
 
       <div className="row">
         <div className="eight columns">
-          <form action="" method="post" id="contactForm" name="contactForm">
-            <fieldset>
-              {resumeData.contactUsForm &&
-                resumeData.contactUsForm.map((item) => {
-                  return (
-                    <div key={item.inputId}>
-                      <label>
-                        {item.labelName}{" "}
-                        <span className={item.className}>
-                          {item.className && "*"}
-                        </span>
-                      </label>
-                      <input
-                        type={item.inputType}
-                        value={item.inputValue}
-                        size={item.inputSize}
-                        id={item.inputId}
-                        name={item.inputName}
-                      />
-                    </div>
-                  );
-                })}
-
-              <div>
-                <label>
-                  Message <span className="required">*</span>
-                </label>
-                <textarea
-                  cols="50"
-                  rows="15"
-                  id="contactMessage"
-                  name="contactMessage"
-                ></textarea>
-              </div>
-
-              <div>
-                <button className="submit">Submit</button>
-                <span id="image-loader">
-                  <img alt="" src="images/loader.gif" />
-                </span>
-              </div>
-            </fieldset>
+          <form method="get" action="mailto:jdcabelloa@outlook.com">
+            <label htmlFor=""></label>
+            <button sytle={{ margin: 0 }} className="button">
+              <i className="fa fa-envelope"></i> Contact Me
+            </button>
           </form>
-
-          <div id="message-warning"> Error boy</div>
-
-          <div id="message-success">
-            <i className="fa fa-check"></i>Your message was sent, thank you!
-            <br />
-          </div>
         </div>
 
         <aside className="four columns footer-widgets">
